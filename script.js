b1671e2bfe013a341b969fe2ba6c91b608d71bcc
Romain-Cesbron <Cesbron.Romain49@gmail.com>
2024-08-27 20:47:24 +0000

    commitment

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-const qr_url_input= document.getElementById('qr_url_input')
-const qr_url = qr_url_input.value
-const qr_space = document.getElementById('qr_space')
+const qr_url_input = document.getElementById('qr_url_input');
+const qr_space = document.getElementById('qr_space');
 
+document.getElementById('generate_bt').addEventListener('click', function () {
+    const qr_url = qr_url_input.value.trim();
+    qr_space.innerHTML = '';
+    createQR(qr_url);
+});
 
-fetch(qr_url).then(function(response){
-    if (response.ok){createQR()
-
-    }
-    else {
-        
-    }
-})
-let qrcode = null
-
-function createQR(){
-    new QRCode(qrcode, {
+function createQR(qr_url) {
+    new QRCode(qr_space, {
         text: qr_url,
         width: 128,
         height: 128,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
     });
-    qr_space.innerHTML=qr_space
-    }
+}
